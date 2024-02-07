76063fa67c10828b09363edb14efe3c3cf7143ac
LiRiK <ggevelurl@gmail.com>
2024-02-07 09:54:33 +0300

    Init

---
+++ src/components/HeroComp/HeroComp.js
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react";
+import { Box, Text, Button, useBreakpointValue } from "@chakra-ui/react";
+
+function HeroComp() {
+    const [currentImage, setCurrentImage] = useState(0);
+    const images = [
+        "/img/1.jpg",
+        "/img/2.jpg",
+        "/img/3.jpg",
+    ];
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentImage(prevImage => (prevImage + 1) % images.length);
+        }, 10000);
+        return () => clearInterval(interval);
+    }, [images.length]);
+
+    const variant = useBreakpointValue({ base: "base", sm: "sm", md: "md", lg: "lg", xl: 'xl', '2xl': '2xl' });
+
+    return (
+        <Box
+            position="relative"
+            height="100vh"
+            backgroundSize="cover"
+            backgroundPosition="center"
+            transition="background-image 1s ease-in-out"
+            backgroundImage={`url(${images[currentImage]})`}
+        >
+            <Box
+                borderTopLeftRadius='60px'
+                borderBottomRightRadius='60px'
+                position="absolute"
+                bgColor='secondary.500'
+                p='4'
+                textAlign="center"
+                w={variant === 'base' || variant === 'sm' ? '100dvw' : '35dvw'}
+                right='0'
+                top={variant === 'base' || variant === 'sm' ? '100dvh' : '70dvh'}
+                color="white"
+            >
+                <Text fontSize="3xl" mb={4} color="primary.500">
+                    Доступно на всех платформах
+                </Text>
+                <Button variant="primary" size={
+                    variant === 'base' ? 'sm' :
+                        variant === 'sm' ? 'md' :
+                            variant === 'md' ? 'md' :
+                                variant === 'lg' ? 'lg'
+                                    : null
+                }>
+                    Узнать больше
+                </Button>
+            </Box>
+        </Box>
+    );
+}
+
+export default HeroComp;
---
+++ src/theme.js
@@ -0,0 +1,67 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+    colors: {
+        primary: {
+            500: "#000000",
+        },
+        secondary: {
+            500: "#F8F200",
+        },
+    },
+    fonts: {
+        heading: "Alegreya, sans-serif;",
+        body: "Roboto, sans-serif",
+    },
+    space: {
+        2: "8px",
+        4: "16px",
+    },
+    components: {
+        Button: {
+            // Переопределение базового стиля кнопки
+            baseStyle: {
+                fontWeight: "bold",
+                borderRadius: "md",
+            },
+            // Стили для различных размеров кнопки
+            sizes: {
+                md: {
+                    fontSize: "md",
+                    padding: "12px 24px",
+                },
+                lg: {
+                    fontSize: "lg",
+                    padding: "16px 32px",
+                },
+            },
+            // Варианты цветов кнопки (например, primary, secondary)
+            variants: {
+                primary: {
+                    bg: "primary.500",
+                    color: "white",
+                    _hover: {
+                        bg: "#333",
+                    },
+                },
+                secondary: {
+                    bg: "secondary.500",
+                    color: "black",
+                    _hover: {
+                        bg: "gray.600",
+                    },
+                },
+            }
+        }
+    },
+    breakpoints: {
+        base: "1px",
+        sm: "320px",
+        md: "768px",
+        lg: "1024px",
+        xl:'400000px',
+        '2xl': '500000px'
+    },
+});
+
+export default theme;
